b9cd980a20f2e611a72c225041f1109a61b29527
Sharon Reshef <sharonreshef89@gmail.com>
2019-10-09 19:21:11 +0300

    setting up to push to heroku

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const cors = require('cors');
+require('dotenv').config()
 const connectDB = require('./config/db');
 
 var indexRouter = require('./routes/index');
--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const config = require('config');
 const db = config.get('mongoURI');
-mongoose.connect(db, { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || db, { useNewUrlParser: true });
 const connectDB = async () => {
   try {
     await mongoose.connect(db, { useNewUrlParser: true, useCreateIndex: true });
