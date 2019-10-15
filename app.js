d0a8654c63e7dbfefdb2300f8eb17b930b6c58a0
Sharon Reshef <sharonreshef89@gmail.com>
2019-10-15 12:14:39 +0300

    add changes for deployment"

--- app.js
+++ app.js
@@ -27,6 +27,16 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'client/build')));
 
+//Serve static asset in production
+if (process.env.NODE_ENV === 'production') {
+  // set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 app.use('/', indexRouter);
 app.use('/customers', customersRouter);
 app.use('/auth', authRouter);
