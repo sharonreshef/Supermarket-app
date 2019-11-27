8259ed83dc7027e2c8c14df86d991425f72d756e
Sharon Reshef <sharonreshef89@gmail.com>
2019-11-27 13:29:10 +0200

    edit app.js on sever

--- app.js
+++ app.js
@@ -75,7 +75,20 @@ app.use(logger("dev"));
 app.use(express.json({ extended: false }));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+
+app.use(express.static(path.join(__dirname, "client/build")));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
+  //
+  app.get("*", (req, res) => {
+    res.sendfile(path.join((__dirname = "client/build/index.html")));
+  });
+}
+//build mode
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/public/index.html"));
+});
 
 app.use("/", indexRouter);
 app.use("/customers", customersRouter);
